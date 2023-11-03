594542e8f32a23ca988143822497e0103b51406d
JEON <jwodn123@naver.com>
2023-11-03 18:19:24 +0900

    feat : 게시글 수정 기능 추가 및 수정 테스트

---
+++ src/main/java/com/jjw/SpringBoot/test/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.jjw.SpringBoot.test.web.dto;
+
+import com.jjw.SpringBoot.test.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
