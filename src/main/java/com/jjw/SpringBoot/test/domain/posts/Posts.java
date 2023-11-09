8c011efcb43495df76e33c6e29094e160fc6a281
JEON <jwodn123@naver.com>
2023-11-09 15:38:12 +0900

    feat : JPA Audition을 이용하여 등록/수정 시간 자동화 및 테스트

--- src/main/java/com/jjw/SpringBoot/test/domain/posts/Posts.java
+++ src/main/java/com/jjw/SpringBoot/test/domain/posts/Posts.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter // 클래스 내 모든 필드의 Getter 메소드를 자동생성.
 @NoArgsConstructor // 기본 생성자 자동 추가, public Posts() {}와 같은 효과.
 @Entity // 테이블과 링크될 클래스임을 나타냄, 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭합니다.
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 PK 필드를 나타냄
     // @GeneratedValue
--- src/main/java/com/jjw/SpringBoot/test/web/HelloController.java
+++ src/main/java/com/jjw/SpringBoot/test/web/HelloController.java
@@ -13,8 +13,7 @@ public class HelloController {
 
     // @GetMapping
     // HTTP 메서드인 Get 요청을 받을 수 있는 API를 만들어 준다.
-    // 예전에는 @RequestMapping(method = RequestMethod.GET)으로 사용되었지만 이제 이 프로젝트는 /hello로 요청이 오면 문자열 hello로
-    // 요청이 오면 문자열 hello를 반환하는 기능을 가지게 되었다.
+    // 예전에는 @RequestMapping(method = RequestMethod.GET)으로 사용되었지만 이제 이 프로젝트는 /hello로 요청이 오면 문자열 hello를 반환하는 기능을 가지게 되었다.
     @GetMapping("/hello")
     public String hello() {
         return "hello";
