8b0f636a98836e95802954e87f69498ca74cb83a
JEON <jwodn123@naver.com>
2023-11-11 15:53:42 +0900

    feat : 게시글 수정/삭제 화면 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -5,6 +5,8 @@ var main = {
             _this.save();
         });
 
+        // $('#btn-update').on('click')
+        // btn-update란 id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트를 등록함.
         $('#btn-update').on('click', function () {
             _this.update();
         });
@@ -33,6 +35,7 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
+    // update : function() -> 신규로 추가될 update function 입니다.
     update : function () {
         var data = {
             title: $('#title').val(),
@@ -42,7 +45,13 @@ var main = {
         var id = $('#id').val();
 
         $.ajax({
+            // type : 'PUT'
+            // 여러 HTTP Method 중 PUT 메소드를 선택.
+            // PostApiController에 있는 API에 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 합니다. 참고로 이는 REST 규약에 맞게 설정된 것임.
+            // REST에서 CRUD는 다음과 같이 HTTP Method에 매핑된다.
+            // 생성 - POST, 읽기 - GET, 수정 - PUT, 삭제 - DELETE
             type: 'PUT',
+            // url: '/api/v1/posts/'+id -> 어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가합니다.
             url: '/api/v1/posts/'+id,
             dataType: 'json',
             contentType:'application/json; charset=utf-8',
