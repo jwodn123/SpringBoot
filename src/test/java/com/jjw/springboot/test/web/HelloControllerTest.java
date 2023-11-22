57a1ad52a81d5db0e32fc29fe67ed35fa7cb2dee
JEON <jwodn123@naver.com>
2023-11-22 16:56:26 +0900

    feat : 기존 테스트에 시큐리티 적용으로 문제가 되는 부분 해결

--- src/test/java/com/jjw/springboot/test/web/HelloControllerTest.java
+++ src/test/java/com/jjw/springboot/test/web/HelloControllerTest.java
@@ -1,9 +1,14 @@
 package com.jjw.springboot.test.web;
 
+import com.jjw.SpringBoot.test.config.auth.SecurityConfig;
+import com.jjw.SpringBoot.test.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -25,7 +30,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 단, @Service, @Component, @Repository 등은 사용할 수 있음.
 // 여기서는 컨트롤러만 사용하기 때문에 선언함.
 // @WebMvcTest의 경우 JPA 기능이 작동하지 않는다.
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     // @Autowired
@@ -35,6 +44,7 @@ public class HelloControllerTest {
     // 스프링 MVC 테스트의 시작점, 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있음.
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -50,6 +60,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
